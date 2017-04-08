8d0ca944e933273329f9e2db238c2ad09c1f48c3
Yuriy <bo20202@yandex.ru>
2017-04-08 12:00:21 +0000

    bot improvements

--- plugins/server_status.py
+++ plugins/server_status.py
@@ -9,32 +9,31 @@ import asyncio
 class ServerStatus:
     def __init__(self, bot):
         self.bot = bot
-        self.server = setup_file["server"]
+        self.servers = setup_file["servers"]
         self.byond = byond.Byond()
+        self.allowed_channels = setup_file["allowed_channels"]
         self.monitoring = False
-    
-    @commands.command(name='info')
-    async def server_info(self):
-        info = self.get_server_info()
-        message = self.build_status(info)
-        await self.bot.say(embed=message)
     
     @commands.command(name='start', pass_context=True)
     @commands.check(server_admin)
     async def monitor(self, context):
         if not self.monitoring:
-            if context.message.channel.id == "288013065499443200" or context.message.channel.id == "286943853033029632":
+            if context.message.channel.id in self.allowed_channels:
                 self.monitoring = True
                 await self.start_monitoring(context)
                 
     
     async def start_monitoring(self, context):
-        msg = await self.bot.say(embed=self.build_status(self.get_server_info()))
+        messages = {}
+        for server in self.servers:
+            status = self.build_status(self.get_server_info(server))
+            msg = await self.bot.say(embed=status)
+            messages[server] = msg
         print('Started monitoring.')
-        await self.bot.say('Started monitoring')
         while self.monitoring:
-            new_info = self.build_status(self.get_server_info())
-            await self.bot.edit_message(msg, embed=new_info)
+            for server in self.servers:
+                new_info = self.build_status(self.get_server_info(server))
+                await self.bot.edit_message(messages[server], embed=new_info)
             await asyncio.sleep(1)
             
     @commands.command(name='stop')
@@ -44,12 +43,18 @@ class ServerStatus:
         await self.bot.say('Stopped monitoring')
         print('Stopped monitoring')
     
-    def get_server_info(self):
+    def get_server_info(self, server):
         try:
-            status = self.byond.request_topic(self.server['domain'], self.server['port'], "status")
-            info = {'players': status['players'], 'round_duration': status["roundduration"], 'admins': status["admins"]}
+            status = self.byond.request_topic(server['domain'], server['port'], "status")
+            print(status)
+            server_url = self.construct_server_url(server['domain'], server['port'])
+            info = {'players': status['players'], 'round_duration': status["roundduration"], 'admins': status["admins"], 'address': server_url}
+            
         except discord.HTTPException:
             info = "Server is offline."
+            
+        except TypeError:
+            info = "Server is offline"
         return info
     
     def build_status(self, info):
@@ -58,14 +63,14 @@ class ServerStatus:
             emb.add_field(name='Server status', value='Server is online!', inline=False)
             emb.add_field(name='Players', value=info["players"])
             emb.add_field(name='Admins', value=info["admins"])
-            emb.add_field(name='Server address', value=self.construct_server_url(), inline=False)
+            emb.add_field(name='Server address', value=info['address'], inline=False)
         else:
             emb.add_field(name='Server status', value=info)
-        
         return emb
         
-    def construct_server_url(self):
-        return "byond://{0}:{1}".format(self.server['domain'], self.server['port'])
+        
+    def construct_server_url(self, domain, port):
+        return "byond://{0}:{1}".format(domain, port)
 
 def setup(bot):
     bot.add_cog(ServerStatus(bot))
